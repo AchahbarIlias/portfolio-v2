39bcde37aff45ada42ff783fbcd56d89e52e160e
AchahbarIlias <ilias.achahbar@student.ucll.be>
2022-01-10 02:04:39 +0100

    projects grid fixed

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -1,67 +1,69 @@
 <template>
-<div class="container flex md:flex-row md:mt-0 mt-20 flex-col mx-auto px-8 md:px-14 lg:px-24 w-full md:space-y-0 space-y-8">
-<div class="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-auto text-white gap-4">
+<div class="container mx-auto px-8 md:px-14 lg:px-24 w-full mt-20">
+<div class="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 text-white gap-2">
 
-        <card class="w-full mx-auto">
-        <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
-          <p class="mx-auto">Project Picture</p>
-        </span>
-        <span class="flex flex-col p-4 text-black bg-white h-fill rounded-b-md">
+        <card class="w-full mx-auto h-full">
+            <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
+          <p class="mx-auto">Bones Hardware</p>
+            </span>
+            <div class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
           <p class="text-xl">Computer Webshop</p>
-          <div class="flex flex-row items-center space-x-4 mt-2">
+          <span class="flex flex-row items-center space-x-2 mt-2">
             <img class="h-full w-8" src="../assets/php.svg" alt="">
             <img class="h-full w-8" src="../assets/html.svg" alt="">
             <img class="h-full w-8" src="../assets/css.svg" alt="">
             <img class="h-full w-8" src="../assets/mysql.svg" alt="">
-          </div>
-        </span>
+          </span>
+            </div>
         </card>
 
-                <card class="w-full mx-auto">
+                <card class="w-full mx-auto h-full">
         <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
-          <p class="mx-auto">Project Picture</p>
+          <p class="mx-auto">Payment System</p>
         </span>
-        <span class="flex flex-col p-4 text-black bg-white h-fill rounded-b-md">
+        <span class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
           <p class="text-xl">Payment System</p>
-          <div class="flex flex-row items-center space-x-4 mt-2">
+          <div class="flex flex-row items-center space-x-2 mt-2">
             <img class="h-full w-8" src="../assets/java.svg" alt="">
           </div>
         </span>
         </card>
 
         
-                <card class="w-full mx-auto">
+                <card class="w-full mx-auto h-full">
         <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
-          <p class="mx-auto">Project Picture</p>
+          <p class="mx-auto">Gomoku</p>
         </span>
-        <span class="flex flex-col p-4 text-black bg-white h-fill rounded-b-md">
+        <span class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
           <p class="text-xl">Gomoku Game</p>
-          <div class="flex flex-row items-center space-x-4 mt-2">
+          <div class="flex flex-row items-center space-x-2 mt-2">
             <img class="h-full w-8" src="../assets/csharp.svg" alt="">
           </div>
         </span>
         </card>
 
-                <card class="w-full mx-auto">
+                <card class="w-full mx-auto h-full">
         <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
-          <p class="mx-auto">Project Picture</p>
+          <p class="mx-auto">J&M United</p>
         </span>
-        <span class="flex flex-col p-4 text-black bg-white h-fill rounded-b-md">
+        <span class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
           <p class="text-xl">Animal Shelter Webshop</p>
-          <div class="flex flex-row items-center space-x-4 mt-2">
+          <div class="flex lg:flex-nowrap flex-wrap flex-row items-center space-x-2 mt-2">
             <img class="h-full w-8" src="../assets/elixir.svg" alt="">
             <img class="h-full w-8" src="../assets/phoenix.svg" alt="">
+            <img class="h-full w-8" src="../assets/html.svg" alt="">
+            <img class="h-full w-8" src="../assets/css.svg" alt="">
           </div>
         </span>
         </card>
 
-                <card class="w-full mx-auto">
+                <card class="w-full mx-auto h-full">
         <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
-          <p class="mx-auto">Project Picture</p>
+          <p class="mx-auto">Discord Picture</p>
         </span>
-        <span class="flex flex-col p-4 text-black bg-white h-fill rounded-b-md">
+        <span class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
           <p class="text-xl">Discord Bot</p>
-          <div class="flex flex-row items-center space-x-4 mt-2">
+          <div class="flex lg:flex-nowrap flex-wrap flex-row items-center space-x-2 mt-2">
             <img class="h-full w-8" src="../assets/javascript.svg" alt="">
             <img class="h-full w-8" src="../assets/node.svg" alt="">
             <img class="h-full w-8" src="../assets/mysql.svg" alt="">
@@ -69,16 +71,37 @@
         </span>
         </card>
 
-                        <card class="w-full mx-auto">
+        <card class="w-full mx-auto h-full">
         <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
-          <p class="mx-auto">Project Picture</p>
+          <p class="mx-auto">You're using it right now</p>
         </span>
-        <span class="flex flex-col p-4 text-black bg-white h-fill rounded-b-md">
+        <span class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
           <p class="text-xl">Portfolio</p>
-          <div class="flex flex-row items-center space-x-4 mt-2">
+          <div class="flex flex-wrap flex-row items-center md:space-x-0 space-x-2 mt-2">
             <img class="h-full w-8" src="../assets/javascript.svg" alt="">
             <img class="h-full w-8" src="../assets/tailwind.svg" alt="">
             <img class="h-full w-8" src="../assets/vue.svg" alt="">
+            <img class="h-full w-8" src="../assets/html.svg" alt="">
+            <img class="h-full w-8" src="../assets/css.svg" alt="">
+          </div>
+        </span>
+        </card>
+
+        <card class="w-full mx-auto h-full lg:col-span-2 md:col-span-3">
+        <span class="flex items-center bg-purple-300 w-full h-52 rounded-t-md">
+          <p class="mx-auto">Digid</p>
+        </span>
+        <span class="flex flex-col p-4 text-black bg-white h-52 rounded-b-md">
+          <p class="text-xl">Electronic File's App</p>
+          <div class="flex lg:flex-nowrap flex-wrap flex-row items-center space-x-2 mt-2">
+            <img class="h-full w-8" src="../assets/flutter.svg" alt="">
+            <img class="h-full w-8" src="../assets/dart.svg" alt="">
+            <img class="h-full w-8" src="../assets/node.svg" alt="">
+            <img class="h-full w-8" src="../assets/azure.svg" alt="">
+            <img class="h-full w-8" src="../assets/mongodb.svg" alt="">
+            <img class="h-full w-8" src="../assets/docker.svg" alt="">
+            <img class="h-full w-8" src="../assets/heroku.svg" alt="">
+            <img class="h-full w-8" src="../assets/javascript.svg" alt="">
           </div>
         </span>
         </card>
